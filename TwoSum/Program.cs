feedcc2c6de4bc802677043441ca1b92758220a8
Charlie Lipperd <lipperd@hotmail.com>
2023-06-05 15:49:01 -0400

    push

---
+++ TwoSum/Program.cs
@@ -0,0 +1,9 @@
+﻿using TwoSumProblem;
+
+Solution solution = new Solution();
+
+int[] nums = { 2, 5, 5, 11 };
+int target  = 10;
+
+int[] answer = solution.TwoSum(nums, target);
+Console.WriteLine($"The answer is [{answer[0]}, {answer[1]}]");
---
+++ TwoSum/Solution.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoSumProblem
+{
+    public class Solution
+    {
+            public int[] TwoSum(int[] nums, int target)
+            {
+                for (int i = 0; i < nums.Length; i++)
+                {
+                    for (int x = i+1; x < nums.Length; x++)
+                    {
+                    if ((nums[i] + nums[x]) == target)
+                        {
+                        int[] output = { i, x };
+                        return output;
+                        }
+                        
+                    }
+                }
+                return new int[0];
+            }   
+    }
+}
